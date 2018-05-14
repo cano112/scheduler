15256aa923a97ab53ea7f694bd3b1fd3c09168b5
Kamil Noster <kamil.noster@gmail.com>
2018-05-14 20:30:19 +0200

    Move email to Person class

--- sp_model/src/main/java/pl/edu/agh/wiet/studiesplanner/model/data/Person.java
+++ sp_model/src/main/java/pl/edu/agh/wiet/studiesplanner/model/data/Person.java
@@ -3,6 +3,7 @@ package pl.edu.agh.wiet.studiesplanner.model.data;
 public abstract class Person {
     private final String firstName;
     private final String surname;
+    private String email;
 
     protected Person(String firstName, String surname) {
         this.firstName = firstName;
@@ -12,12 +13,21 @@ public abstract class Person {
     public String getFullName() {
         return firstName + " " + surname;
     }
+
     public String getFirstName() {
         return firstName;
     }
 
     public String getSurname() {
         return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
@@ -27,14 +37,16 @@ public abstract class Person {
 
         Person person = (Person) o;
 
-        if (!firstName.equals(person.firstName)) return false;
-        return surname.equals(person.surname);
+        if (firstName != null ? !firstName.equals(person.firstName) : person.firstName != null) return false;
+        if (surname != null ? !surname.equals(person.surname) : person.surname != null) return false;
+        return email != null ? email.equals(person.email) : person.email == null;
     }
 
     @Override
     public int hashCode() {
-        int result = firstName.hashCode();
-        result = 31 * result + surname.hashCode();
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
         return result;
     }
 }
--- sp_model/src/main/java/pl/edu/agh/wiet/studiesplanner/model/data/Student.java
+++ sp_model/src/main/java/pl/edu/agh/wiet/studiesplanner/model/data/Student.java
@@ -5,47 +5,21 @@ package pl.edu.agh.wiet.studiesplanner.model.data;
  */
 public class Student extends Person {
     private final String id;
-    private final String email;
     private final StudentsGroup group;
 
     public Student(String firstName, String surname, String id, String email, StudentsGroup group) {
         super(firstName, surname);
         this.id = id;
-        this.email = email;
         this.group = group;
+        setEmail(email);
     }
 
     public String getId() {
         return id;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
     public StudentsGroup getGroup() {
         return group;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-
-        Student student = (Student) o;
-
-        if (!id.equals(student.id)) return false;
-        if (!email.equals(student.email)) return false;
-        return group.equals(student.group);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + id.hashCode();
-        result = 31 * result + email.hashCode();
-        result = 31 * result + group.hashCode();
-        return result;
-    }
 }
