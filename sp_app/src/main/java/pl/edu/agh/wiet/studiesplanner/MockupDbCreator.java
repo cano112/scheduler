44daa3b9169ce167e3badf3821ea93188a2fcf4c
Kamil Noster <kamil.noster@gmail.com>
2018-06-11 17:16:58 +0200

    Dev/prod config

--- sp_app/src/main/java/pl/edu/agh/wiet/studiesplanner/MockupDbCreator.java
+++ sp_app/src/main/java/pl/edu/agh/wiet/studiesplanner/MockupDbCreator.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import pl.edu.agh.wiet.studiesplanner.model.parser.google.GoogleDocsParticipantLink;
@@ -17,6 +18,7 @@ import javax.transaction.Transactional;
  * Temporary class for filling db with mockup data
  */
 @Component
+@Profile("dev")
 @Transactional
 public class MockupDbCreator implements ApplicationListener<ContextRefreshedEvent> {
 
--- sp_security/src/main/java/pl/edu/agh/wiet/studiesplanner/config/AuthService.java
+++ sp_security/src/main/java/pl/edu/agh/wiet/studiesplanner/config/AuthService.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.wiet.studiesplanner.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,6 +12,12 @@ public class AuthService implements UserDetailsService {
 
     private final BCryptPasswordEncoder encoder;
 
+    @Value("${studiesplanner.security.login}")
+    private String login;
+
+    @Value("${studiesplanner.security.password}")
+    private String password;
+
     @Autowired
     public AuthService(BCryptPasswordEncoder encoder) {
         this.encoder = encoder;
@@ -18,7 +25,7 @@ public class AuthService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return new User(encoder.encode("haslo"), "admin");
+        return new User(encoder.encode(password), login);
     }
 
 }
